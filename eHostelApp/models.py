a87c15583366e997f509e56e6b77a3ec1b1c2432
Gaurav Singh <gauravsingh6225@gmail.com>
2020-03-30 17:21:04 +0530

    dummy commit

--- eHostelApp/models.py
+++ eHostelApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Allstudents(models.Model):
+    join_year   =  models.InterField(default = 2017)
+    roll_no     =  models.CharField(max_length = 10, primary_key = True,unique = True)
+
+class Hostel(models.Model):
+    pass
+
+class St_details(models.Model):
+    pass
+
+class Admin(models.Model):
+    pass
+
+class Room(models.Model):
+    pass
+
+class Swap(models.Model):
+    pass
+
+class teammate(models.Models):
+    pass
--- eHostelApp/urls.py
+++ eHostelApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('index/', views.index, name = 'index'),
+#   path('index/', views.index, name = 'index'),
 #    path('login/', views.login, name = 'login'),
 #    path('login_warden/', views.login_warden, name = 'login_warden'),
 #    path('register/', views.register, name = 'register'),
